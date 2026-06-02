8563b080e806bf19592fff27b9d1902f73d52ceb
lucho <nicolasluciano1425@gmail.com>
2026-06-02 16:15:38 -0300

    Renombre y primitiva Lista - parte 2

--- Proyecto_Caravana_del_Desierto/headers/jugador.h
+++ Proyecto_Caravana_del_Desierto/headers/jugador.h
@@ -1,6 +1,15 @@
 #ifndef JUGADOR_H_INCLUDED
 #define JUGADOR_H_INCLUDED
 
+#include "lista.h"
 
+typedef struct
+{
+    tNodoLista* posicion;
+    int vidas;
+    int puntos;
+    int protegido;
+    int pierdeTurno;
+} Jugador;
 
 #endif // JUGADOR_H_INCLUDED
