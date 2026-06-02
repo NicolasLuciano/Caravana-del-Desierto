141fe63cb27cd61bebcde2cafd6065d9cc80a07f
RamaFariello <ramiro_fariello@hotmail.com>
2026-06-02 16:43:29 -0300

    Implementación del dado de 6 caras con límite configurable en C

---
+++ Proyecto_Caravana_del_Desierto/headers/dado.h
@@ -0,0 +1,6 @@
+#ifndef DADO_H_INCLUDED
+#define DADO_H_INCLUDED
+
+int tirar_dado(int limite);
+
+#endif // DADO_H_INCLUDED
---
+++ Proyecto_Caravana_del_Desierto/sources/dado.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../headers/dado.h"
+#include "../headers/constantesymacros.h"
+
+
+
+int tirar_dado(int limite) {
+    if (limite < 1) return LIMITE_INVALIDO;
+    return rand() % limite + 1;
+}
